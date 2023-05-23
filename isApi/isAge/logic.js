fd2a5432ef8e8d93369a6fa1cf52bb6a7d0be2a9
Data <110648069+data0504@users.noreply.github.com>
2023-05-23 14:22:55 +0800

    Update Revise ultimate password

--- isApi/isAge/logic.js
+++ isApi/isAge/logic.js
@@ -1,40 +1,69 @@
+var limitBig = 100, limitSmall = 1
+
 function getText()
 {
     const input = document.getElementById("myInput");
     const text = input.value;
-    return parseInt(text); 
+    return parseInt(text);
 }
-function check(ageClient, ageServer, result)
+function saveInterval(ageClient, ageServer)
+{
+    if(ageClient < ageServer)
+    {
+        limitSmall = ageClient
+    }
+    if(ageClient > ageServer)
+    {
+        limitBig = ageClient
+    }
+}
+function checkoutText(ageClient, ageServer, result)
 {
     if (isNaN(ageClient) || isNaN(ageServer))
     {
         result.textContent = `結果: 參數為 ${ageClient}，請重新輸入有效的數字`;
         return false
     }
-
+    if (ageClient<limitSmall || ageClient>limitBig)
+    {
+        result.textContent = `結果: 請輸入 ${limitSmall} ~ ${limitBig} 數字, 謝謝。`;
+        return false
+    }
+    return true
+}
+function checkoutReslut(ageClient, ageServer, result)
+{
     let parseResult = ageClient === ageServer
     if(!parseResult)
     {
-        if (ageClient > ageServer)
+        if(ageClient < ageServer && ageServer< limitBig)
         {
-            result.textContent = "結果: 再猜小點"
+            saveInterval(ageClient, ageServer)
+            result.textContent = `結果: ${limitSmall} ~ ${limitBig}`
         }
-        if (ageClient < ageServer)
+
+        if(ageClient > ageServer && ageServer < limitBig)
         {
-            result.textContent = "結果: 再猜大點"
+            saveInterval(ageClient, ageServer)
+            result.textContent = `結果: ${limitSmall} ~ ${limitBig}`
         }
         return false
     }
     return true
 }
 
+function check(ageClient, ageServer, result)
+{
+    if (!checkoutText(ageClient, ageServer, result)) return false;
+    if (!checkoutReslut(ageClient, ageServer, result)) return false;
+    return true
+}
 function parseAge(ageClient, ageServer, result)
 {
     result.textContent = "結果: 比較中"
     if (!check(ageClient, ageServer, result)) return;
     result.textContent = "結果: 答對了"
 }
-
 function callAgeAPI() {
     const ageUrl="https://api.agify.io/?name=danny"
     fetch(ageUrl)
